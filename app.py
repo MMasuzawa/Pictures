e9e13ee9371827158de0865dea0324489787f2c0
MMasuzawa <122146756+MMasuzawa@users.noreply.github.com>
2023-01-09 20:06:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if img_file is not None:
             if result[0]=="犬" or "猫":
              #st.write(str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。")
               st.write(result[0] + "顔です。")
-           result[0]!="犬" and "猫":
+          if  (result[0]!="犬") and (result[0]!= "猫"):
            st.write("どちらでもありません")
 
         # 円グラフの表示
