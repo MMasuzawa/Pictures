a34153d18f8e26b07bd94c98775fc11e7392f334
MMasuzawa <122146756+MMasuzawa@users.noreply.github.com>
2023-01-09 19:48:44 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ if img_file is not None:
              #st.write(str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。")
               st.write(result[0] + "顔です。")
 
-              st.write[0]
 
         # 円グラフの表示
         #pie_labels = [result[1] for result in results[:n_top]]
